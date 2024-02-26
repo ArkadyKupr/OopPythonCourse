1472f6224a2cbee82fafff3f249d40a584c16ef6
Аркадий <a-kupryakov@mail.ru>
2024-02-26 23:17:32 +0300

    Написал функцию суммирования двух векторов с созданием трерьего

--- Vector/vector.py
+++ Vector/vector.py
@@ -47,5 +47,109 @@ class Vector:
         return f"{numbers_string}"
 
 
+def get_sum_vector(vector_1, vector_2):
+    vector_1_length = len(vector_1)
+    vector_2_length = len(vector_2)
+
+    if vector_1_length >= vector_2_length:
+        for i, value_1 in enumerate(vector_1):
+            value_1 = vector_1[i]
+
+            if i < vector_2_length:
+
+                for j, value_2 in enumerate(vector_2):
+                    if i == j:
+                        value_2 = vector_2[j]
+                        value_1 += value_2
+                        vector_1[i] = value_1
+
+    else:
+        length_difference = vector_2_length - vector_1_length
+
+        for i in range(length_difference):
+            vector_1.append(0)
+
+        for i, value_1 in enumerate(vector_1):
+            value_1 = vector_1[i]
+
+            for j, value_2 in enumerate(vector_2):
+                if i == j:
+                    value_1 += vector_2[j]
+                    vector_1[i] = value_1
+
+    return vector_1
+
+
+def get_diff_vector(vector_1, vector_2):
+    vector_1_length = len(vector_1)
+    vector_2_length = len(vector_2)
+
+    if vector_1_length >= vector_2_length:
+        for i, value_1 in enumerate(vector_1):
+            value_1 = vector_1[i]
+
+            if i < vector_2_length:
+
+                for j, value_2 in enumerate(vector_2):
+                    if i == j:
+                        value_2 = vector_2[j]
+                        value_1 -= value_2
+                        vector_1[i] = value_1
+
+    else:
+        length_difference = vector_2_length - vector_1_length
+
+        for i in range(length_difference):
+            vector_1.append(0)
+
+        for i, value_1 in enumerate(vector_1):
+            value_1 = vector_1[i]
+
+            for j, value_2 in enumerate(vector_2):
+                if i == j:
+                    value_1 -= vector_2[j]
+                    vector_1[i] = value_1
+
+    return vector_1
+
+
+def get_sum_new_vector(vector_1, vector_2):
+    vector_1_length = len(vector_1)
+    vector_2_length = len(vector_2)
+
+    max_length = vector_1_length if vector_1_length > vector_2_length else vector_2_length
+
+    sum_vector = []
+    for i in range(max_length):
+        sum_vector.append(0)
+
+    if vector_1_length > vector_2_length:
+        length_difference = vector_1_length - vector_2_length
+
+        for i in range(length_difference):
+            vector_2.append(0)
+    else:
+        length_difference = vector_2_length - vector_1_length
+
+        for i in range(length_difference):
+            vector_1.append(0)
+
+    for i, sum_value in enumerate(sum_vector):
+
+        for j, value_1 in enumerate(vector_1):
+            value_1 = vector_1[j]
+
+            for k, value_2 in enumerate(vector_2):
+                value_2 = vector_2[k]
+
+                if i == j and j == k:
+                    sum_vector[i] = value_1 + value_2
+
+    return sum_vector
+
+
 user_vector = Vector([1, 2, 3])
+user_vector_1 = Vector([1, 2, 3])
 print(user_vector)
+
+print(get_sum_new_vector([1, 2, 7, 3, 9], [1, 5, 5, 9, 3, 8]))
