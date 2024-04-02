1d19d4606bd81fd94e17a9f1be0fbfc53f0dfb0c
Аркадий <a-kupryakov@mail.ru>
2024-04-02 22:08:23 +0300

    исправления до 7 пункта

--- Range/range.py
+++ Range/range.py
@@ -1,111 +1,62 @@
 class Range:
-    def __init__(self, start_1, end_1, start_2, end_2):
-        self.__start_1 = start_1
-        self.__end_1 = end_1
-        self.__start_2 = start_2
-        self.__end_2 = end_2
+    def __init__(self, start, end):
+        self.__start = start
+        self.__end = end
 
     @property
-    def start_1(self):
-        return self.__start_1
+    def start(self):
+        return self.__start
 
-    @start_1.setter
-    def start_1(self, start_1):
-        self.__start_1 = start_1
-
-    @property
-    def end_1(self):
-        return self.__end_1
-
-    @end_1.setter
-    def end_1(self, end_1):
-        self.__end_1 = end_1
+    @start.setter
+    def start(self, start):
+        self.__start = start
 
     @property
-    def start_2(self):
-        return self.__start_2
+    def end(self):
+        return self.__end
 
-    @start_2.setter
-    def start_2(self, start_2):
-        self.__start_2 = start_2
+    @end.setter
+    def end(self, end):
+        self.__end = end
 
-    @property
-    def end_2(self):
-        return self.__end_2
-
-    @end_2.setter
-    def end_2(self, end_2):
-        self.__end_2 = end_2
-
-    def get_intersection_range(self):
-        if self.__end_1 <= self.__start_2 or self.__end_2 <= self.__start_1:
+    def get_intersection(self, other):
+        if self.__end <= other.__start or other.__end <= self.__start:
             return None
         # один интервал входит в другой
-        elif self.__start_2 >= self.__start_1 and self.__end_2 <= self.__end_1:
-            return [self.__start_2, self.__end_2]
-        elif self.__start_1 >= self.__start_2 and self.__end_1 <= self.__end_2:
-            return [self.__start_1, self.__end_1]
+        elif other.__start >= self.__start and other.__end <= self.__end:
+            return [other.__start, other.__end]
+        elif self.__start >= other.__start and self.__end <= other.__end:
+            return [self.__start, self.__end]
         # интервалы пересекаются
-        elif self.__start_1 < self.__start_2:
-            return [self.__start_2, self.__end_1]
+        elif self.__start < other.__start:
+            return [other.__start, self.__end]
         else:
-            return [self.__start_1, self.__end_2]
+            return [self.__start, other.__end]
 
-    def get_merged_range(self):
-        if self.__end_1 < self.__start_2 or self.__end_2 < self.__start_1:
-            return [(self.__start_1, self.__end_1), (self.__start_2, self.__end_2)]
+    def get_union(self, other):
+        if self.__end < other.__start or other.__end < self.__start:
+            return [(self.__start, self.__end), (other.__start, other.__end)]
         # один интервал входит в другой
-        elif self.__start_2 >= self.__start_1 and self.__end_2 <= self.__end_1:
-            return [(self.__start_1, self.__end_1)]
-        elif self.__start_1 >= self.__start_2 and self.__end_1 <= self.__end_2:
-            return [(self.__start_2, self.__end_2)]
+        elif other.__start >= self.__start and other.__end <= self.__end:
+            return [(self.__start, self.__end)]
+        elif self.__start >= other.__start and self.__end <= other.__end:
+            return [(other.__start, other.__end)]
         # интервалы пересекаются
-        elif self.__start_1 < self.__start_2:
-            return [self.__start_1, self.__end_2]
+        elif self.__start < other.__start:
+            return [self.__start, other.__end]
         else:
-            return [self.__start_2, self.__end_1]
+            return [other.__start, self.__end]
 
-    def get_subtracted_range(self):
-        if self.__end_1 < self.__start_2 or self.__end_2 < self.__start_1:
-            return [(self.__start_1, self.__end_1), (self.__start_2, self.__end_2)]
+    def get_subtraction(self, other):
+        if self.__end < other.__start or other.__end < self.__start:
+            return [(self.__start, self.__end), (other.__start, other.__end)]
         # один интервал входит в другой
-        elif self.__start_2 >= self.__start_1 and self.__end_2 <= self.__end_1:
-            return [(self.__start_1, self.__start_2), (self.__end_2, self.__end_1)]
-        elif self.__start_1 >= self.__start_2 and self.__end_1 <= self.__end_2:
-            return [(self.__start_2, self.__start_1), (self.__end_1, self.__end_2)]
+        elif other.__start >= self.__start and other.__end <= self.__end:
+            return [(self.__start, other.__start), (other.__end, self.__end)]
+        elif self.__start >= other.__start and self.__end <= other.__end:
+            return [(other.__start, self.__start), (self.__end, other.__end)]
         # интервалы пересекаются
-        elif self.__start_1 < self.__start_2:
-            return [(self.__start_1, self.__start_2), (self.__end_1, self.__end_2)]
+        elif self.__start < other.__start:
+            return [(self.__start, other.__start), (self.__end, other.__end)]
         else:
-            return [(self.__start_2, self.__start_1), (self.__end_2, self.__end_1)]
-
-
-numbers_ranges = Range(1, 10, 10, 20)
-print("Интервал-пересечение двух интервалов:", numbers_ranges.get_intersection_range())
-print("Объединение двух интервалов:", numbers_ranges.get_merged_range())
-print("Разность двух интервалов:", numbers_ranges.get_subtracted_range())
-
-numbers_ranges = Range(10, 20, 1, 10)
-print("Интервал-пересечение двух интервалов:", numbers_ranges.get_intersection_range())
-print("Объединение двух интервалов:", numbers_ranges.get_merged_range())
-print("Разность двух интервалов:", numbers_ranges.get_subtracted_range())
-
-numbers_ranges = Range(10, 13, 11, 15)
-print("Интервал-пересечение двух интервалов:", numbers_ranges.get_intersection_range())
-print("Объединение двух интервалов:", numbers_ranges.get_merged_range())
-print("Разность двух интервалов:", numbers_ranges.get_subtracted_range())
-
-numbers_ranges = Range(10, 15, 11, 13)
-print("Интервал-пересечение двух интервалов:", numbers_ranges.get_intersection_range())
-print("Объединение двух интервалов:", numbers_ranges.get_merged_range())
-print("Разность двух интервалов:", numbers_ranges.get_subtracted_range())
-
-numbers_ranges = Range(11, 15, 14, 119)
-print("Интервал-пересечение двух интервалов:", numbers_ranges.get_intersection_range())
-print("Объединение двух интервалов:", numbers_ranges.get_merged_range())
-print("Разность двух интервалов:", numbers_ranges.get_subtracted_range())
-
-numbers_ranges = Range(14, 119, 11, 15)
-print("Интервал-пересечение двух интервалов:", numbers_ranges.get_intersection_range())
-print("Объединение двух интервалов:", numbers_ranges.get_merged_range())
-print("Разность двух интервалов:", numbers_ranges.get_subtracted_range())
+            return [(other.__start, self.__start), (other.__end, self.__end)]
