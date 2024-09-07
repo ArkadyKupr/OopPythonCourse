0957e9ce082934d94643d189f37184df7ec75127
Аркадий <a-kupryakov@mail.ru>
2024-09-07 21:16:05 +0300

    Исправление задачи

--- graph_task/graph.py
+++ graph_task/graph.py
@@ -1,24 +1,110 @@
+from queue import Queue
+
+
 class Graph:
-    def __init__(self):
-        self.__nodes = {}
+    def __init__(self, graph):
+        if not isinstance(graph, list):
+            raise TypeError(f"Тип {graph} не является list")
 
-    def add_node(self, value):
-        if value not in self.__nodes:
-            self.__nodes[value] = []
+        for row in graph:
+            if not isinstance(row, list):
+                raise TypeError(f"Тип {row} не является list")
 
-    def add_edge(self, from_node, to_node):
-        if from_node in self.__nodes:
-            if to_node in self.__nodes:
-                self.__nodes[from_node].append(to_node)
+        # Проверка размерности и чтобы когда i=j элемент списка имел значение 0
+        rows_quantity = len(graph)
 
-    def print_graph(self):
-        for value in self.__nodes.values():
-            print(value)
+        if rows_quantity == 0:
+            raise ValueError(f"Граф не должен быть пустым")
 
-    def graph_matrix(self):
-        dimension = len(self.__nodes)
+        for row in graph:
+            if len(row) != rows_quantity:
+                raise IndexError(f"Количество столбцов в строке {row}: {len(row)}, "
+                                 f"не равно заданному количеству строк {rows_quantity}")
 
-        matrix = [None] * dimension
+        for i in range(rows_quantity):
+            for j in range(rows_quantity):
+                if i == j:
+                    graph[i][j] = 0
 
-        for element in matrix:
-            element = [False] * dimension
+        self.__graph = graph
+
+    @property
+    def vertex_quantity(self):
+        return len(self.__graph)
+
+    def __repr__(self):
+        rows_list = ""
+
+        for i in range(self.vertex_quantity):
+            for j in range(self.vertex_quantity):
+                rows_list += str(self.__graph[i][j])
+                rows_list += " "
+
+            rows_list += "\n"
+
+        return rows_list
+
+    # Обход в ширину
+    def breadth_first_search(self):
+        visited = [False] * self.vertex_quantity
+
+        # Использование очереди
+        vertexes_queue = Queue()
+
+        for i in range(self.vertex_quantity):
+            if not visited[i]:
+                vertexes_queue.put(self.__graph[i])
+                visited[i] = True
+
+                while not vertexes_queue.empty():
+                    vertex = vertexes_queue.get()
+                    print(vertex)
+
+                    for j in range(self.vertex_quantity):
+                        if not visited[j] and vertex[j] == 1:
+                            vertexes_queue.put(self.__graph[j])
+                            visited[j] = True
+
+                            break
+
+        return visited
+
+    # Обход в глубину без рекурсии
+    def width_first_search(self):
+        visited = [False] * self.vertex_quantity
+
+        stack = []
+
+        for i in range(self.vertex_quantity):
+            if not visited[i]:
+                stack.append(self.__graph[i])
+                visited[i] = True
+
+                while len(stack) > 0:
+                    first_vertex = stack.pop()
+
+                    for j in range(self.vertex_quantity):
+                        if not visited[j] and first_vertex[j] == 1:
+                            first_vertex.append(self.__graph[j])
+                            visited[j] = True
+
+        return visited
+
+    # Обход в глубину c рекурсией
+    def width_first_search_recursive(self):
+        visited = [False] * self.vertex_quantity
+
+        def visit(visited_recursive):
+            for i in range(self.vertex_quantity):
+                if not visited[i]:
+                    print(i)
+                    print(self.__graph[i])
+                    print()
+
+                    visited[i] = True
+
+                    for j in range(self.vertex_quantity):
+                        if self.__graph[i][j] == 1:
+                            visit(visited_recursive)
+
+        visit(visited)
--- graph_task/graph_test.py
+++ graph_task/graph_test.py
@@ -1,10 +1,25 @@
 from graph_task.graph import Graph
 
-graph = Graph()
-graph.add_node("A")
-graph.add_node("B")
-graph.add_node("C")
-graph.add_edge("A", "A")
-graph.add_edge("A", "B")
-graph.add_edge("C", "B")
-graph.print_graph()
+# задаем граф двумерным списком
+user_graph = [[100, 1, 0, 0, 0, 0, 0],
+              [1, 0, 1, 1, 1, 1, 0],
+              [0, 1, 0, 0, 0, 0, 1],
+              [0, 1, 0, 0, 0, 0, 0],
+              [0, 1, 0, 0, 0, 1, 0],
+              [0, 1, 0, 0, 1, 0, 1],
+              [0, 0, 1, 0, 0, 1, 0]]
+
+graph = Graph(user_graph)
+print(graph)
+print()
+
+print("Обход в ширину")
+print(graph.breadth_first_search())
+print()
+
+print("Обход в глубину без рекурсии")
+print(graph.width_first_search())
+print()
+
+print("Обход в глубину с рекурсией")
+print(graph.width_first_search_recursive())
